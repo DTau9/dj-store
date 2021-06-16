e864902e7b656efcb4da8390d59089bb90acaee8
D.Tau <di.tau9@gmail.com>
2021-06-16 19:42:20 +0300

    подправил ширину скролла

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,7 @@ module.exports = {
                 plugins: [
                   ["mozjpeg", {
                     progressive: true,
-                    quality: 100
+                    quality: 70
                   }]
                 ],
               },
